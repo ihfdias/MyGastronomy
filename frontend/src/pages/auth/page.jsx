3551fdbe1e6b4ddb444e6f8d60f495cbd72e1f79
Igor <ihfdias.dev@gmail.com>
2025-11-15 10:00:53 -0300

    auth form complete

--- frontend/src/pages/auth/page.jsx
+++ frontend/src/pages/auth/page.jsx
@@ -1,50 +1,119 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { TextField, Button } from "@mui/material"
-import StyleSheet from "./auth.module.css"
+import Style from "./page.module.css"
+import authServices from "../../services/auth"
 
 export default function Auth() {
     const [formType, setFormType] = useState('login')
+    const [formData, setFormData] = useState(null)
+    const { login, signup, authLoading } = authServices()
+
+    useEffect(() => {
+        setFormData(null)
+    }, [formData])
 
     const handleChangeFormType = () => {
-        if(formType === 'login') {
+        setFormData(null)
+        if (formType === 'login') {
             setFormType('signup')
         } else {
             setFormType('login')
         }
     }
-    
 
-    if(formType === 'login') {
+    const handleFormDataChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+
+        switch (formType) {
+            case 'login':
+                    login(formData)
+                break
+
+            case 'signup':
+                    if(formData.password !== formData.confirmPassword){
+                        console.log("Password do not match")
+                        return
+                    }
+                    signup(formData)
+                break
+        }
+    }
+
+    if (authLoading) {
+        return ( <h1>Loading...</h1> )
+    }
+
+    if (formType === 'login') {
         return (
-            <div className={StyleSheet.authPageContainer}>
+            <div className={Style.authPageContainer}>
                 <h1>Login</h1>
                 <button onClick={handleChangeFormType}>Don't you have an account? Click here</button>
-                <form>
-                    <TextField 
-                    required
-                    label="Email"
-                    type="email"
-                    name="email"
+                <form onSubmit={handleSubmitForm}>
+                    <TextField
+                        required
+                        label="Email"
+                        type="email"
+                        name="email"
+                        onChange={handleFormDataChange}
                     />
-                    <TextField 
-                    required
-                    label="Password"
-                    type="password"
-                    name="password"
+                    <TextField
+                        required
+                        label="Password"
+                        type="password"
+                        name="password"
+                        onChange={handleFormDataChange}
                     />
                     <Button type="submit">Login</Button>
                 </form>
             </div>
 
-    )
-}
+        )
+    }
 
-    if(formType === 'signup') {
+    if (formType === 'signup') {
         return (
-            <>
+            <div className={Style.authPageContainer}>
                 <h1>Signup</h1>
                 <button onClick={handleChangeFormType}>Already have an account? Click here</button>
-            </>
-    )
+                <form onSubmit={handleSubmitForm}>
+                    <TextField
+                        required
+                        label="Fullname"
+                        type="Fullname"
+                        name="Fullname"
+                        onChange={handleFormDataChange}
+                    />
+                    <TextField
+                        required
+                        label="Email"
+                        type="email"
+                        name="email"
+                        onChange={handleFormDataChange}
+                    />
+                    <TextField
+                        required
+                        label="Password"
+                        type="password"
+                        name="password"
+                        onChange={handleFormDataChange}
+                    />
+                    <TextField
+                        required
+                        label="Confirm password"
+                        type="password"
+                        name="confirmPassword"
+                        onChange={handleFormDataChange}
+                    />
+                    <Button type="submit">Signup</Button>
+                </form>
+            </div>
+        )
     }
 }
---
+++ frontend/src/services/auth.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react"
+
+export default function authServices() {
+    const [authLoading, setAuthLOading] = useState(false)
+
+    const url = 'http://localhost:3000/auth'
+
+    const login = (FormData) => {
+        setAuthLOading(true)
+
+        fetch(`${url}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(FormData)
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                if(result.success && result.body.token) {
+                    localStorage.setItem('auth', JSON.stringify({token: result.body.token, user:result.body.user }))
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                setAuthLOading(false)
+            })
+
+    }
+
+    const logout = () => {
+
+    }
+
+    const signup = (FormData) => {
+        setAuthLOading(true)
+
+        fetch(`${url}/signup`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify(FormData)
+        })
+            .then((response) => response.json())
+            .then((result) => {
+               if(result.success && result.body.token) {
+                    localStorage.setItem('auth', JSON.stringify({token: result.body.token, user:result.body.user }))
+                }
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+            .finally(() => {
+                setAuthLOading(false)
+            })
+
+    }
+
+    return { signup, login, logout, authLoading }
+}
